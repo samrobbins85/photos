16b785793594b8a24b0b5e6c8449295efd7a0348
Sam Robbins <samrobbinsgb@gmail.com>
2021-01-27 19:16:11 +0000

    Fix the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,14 +12,14 @@ module.exports = {
 			gridColumn: {
 				1: "1",
 			},
-		},
-		colors: {
-			"black-opaque": "rgba(0, 0, 0, 0.7)",
-		},
-		maxHeight: {
-			"1/4": "25%",
-			"1/2": "50%",
-			"3/4": "75%",
+			colors: {
+				"black-opaque": "rgba(0, 0, 0, 0.7)",
+			},
+			maxHeight: {
+				"1/4": "25%",
+				"1/2": "50%",
+				"3/4": "75%",
+			},
 		},
 	},
 	variants: {},
