41cabf1449124daa4b25fb0b169cf421db114a1b
Sam Robbins <samrobbinsgb@gmail.com>
2021-01-29 15:16:29 +0000

    Add a lightbox, doesn't work perfectly with next image

--- pages/[slug].js
+++ pages/[slug].js
@@ -2,13 +2,22 @@ import { getPage, getSectionPaths } from "../lib/api";
 import CImage from "../components/cimage";
 import Head from "next/head";
 import Link from "next/link";
+import { SRLWrapper } from "simple-react-lightbox";
+const options = {
+	buttons: {
+		showAutoplayButton: false,
+		showDownloadButton: false,
+		showFullscreenButton: false,
+		showThumbnailsButton: false,
+	},
+};
 export default function Gallery({ pagedata }) {
 	return (
 		<>
 			<Head>
 				<title>{pagedata.title} - Sam Robbins Photography</title>
 			</Head>
-			<div className="pt-10 pl-10 text-gray-600 hover:text-gray-700">
+			<div className="pt-6 pl-6 sm:pt-10 sm:pl-10 text-gray-600 hover:text-gray-700">
 				<Link href="/">
 					<a>
 						<svg
@@ -16,16 +25,15 @@ export default function Gallery({ pagedata }) {
 							fill="none"
 							viewBox="0 0 24 24"
 							stroke="currentColor"
-							className="h-6 w-6 inline-block mr-2"
+							className="h-8 w-8"
 						>
 							<path
 								strokeLinecap="round"
 								strokeLinejoin="round"
 								strokeWidth={2}
-								d="M10 19l-7-7m0 0l7-7m-7 7h18"
+								d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
 							/>
 						</svg>
-						<span className="text-lg">Home</span>
 					</a>
 				</Link>
 			</div>
@@ -34,16 +42,21 @@ export default function Gallery({ pagedata }) {
 					{pagedata.title}
 				</h1>
 				<div className="cols-1 sm:cols-2 md:cols-3 lg:cols-4 pt-10">
-					{pagedata.imagesCollection.items.map((x) => (
-						<div className="overflow-hidden mb-8 rounded-lg">
-							<CImage
-								src={x.url}
-								width={x.width}
-								height={x.height}
-								className="rounded-lg overflow-hidden mb-8 pb-0"
-							/>
-						</div>
-					))}
+					<SRLWrapper options={options}>
+						{pagedata.imagesCollection.items.map((x) => (
+							<a href={x.url}>
+								<div className="overflow-hidden mb-8 rounded-lg">
+									<CImage
+										src={x.url}
+										width={x.width}
+										height={x.height}
+										priority={true}
+										className="rounded-lg overflow-hidden mb-8 pb-0"
+									/>
+								</div>
+							</a>
+						))}
+					</SRLWrapper>
 				</div>
 			</div>
 		</>
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import "../styles/index.css";
-
+import SimpleReactLightbox from "simple-react-lightbox";
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<SimpleReactLightbox>
+			<Component {...pageProps} />
+		</SimpleReactLightbox>
+	);
 }
 
 export default MyApp;
